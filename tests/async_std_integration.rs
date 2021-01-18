8bb07e5fff68ca00357348173fefb5217f7bece7
Thane Thomson <connect@thanethomson.com>
2021-01-18 14:00:13 -0500

    Feature-guard each runtime's tests
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- tests/async_std_integration.rs
+++ tests/async_std_integration.rs
@@ -1,16 +1,21 @@
-use async_channel_abs::{App, AsyncStd};
+//! Example app using `async-std`.
 
-#[async_std::test]
-async fn async_std_app() {
-    let (app, driver) = App::<AsyncStd>::new("Hello async-std!");
-    let driver_handle = async_std::task::spawn(async move { driver.run().await });
+#[cfg(feature = "with-async-std")]
+mod async_std_integration {
+    use async_channel_abs::{App, AsyncStd};
 
-    let init_state = app.get_state().await.unwrap();
-    assert_eq!(init_state, "Hello async-std!");
+    #[async_std::test]
+    async fn async_std_app() {
+        let (app, driver) = App::<AsyncStd>::new("Hello async-std!");
+        let driver_handle = async_std::task::spawn(async move { driver.run().await });
 
-    let new_state = app.update_state("Hello again!").await.unwrap();
-    assert_eq!(new_state, "Hello again!");
+        let init_state = app.get_state().await.unwrap();
+        assert_eq!(init_state, "Hello async-std!");
 
-    app.terminate().await.unwrap();
-    let _ = driver_handle.await;
+        let new_state = app.update_state("Hello again!").await.unwrap();
+        assert_eq!(new_state, "Hello again!");
+
+        app.terminate().await.unwrap();
+        let _ = driver_handle.await;
+    }
 }
--- tests/tokio_integration.rs
+++ tests/tokio_integration.rs
@@ -1,18 +1,21 @@
 //! Example app using Tokio.
 
-use async_channel_abs::{App, Tokio};
+#[cfg(feature = "with-tokio")]
+mod tokio_integration {
+    use async_channel_abs::{App, Tokio};
 
-#[tokio::test]
-async fn tokio_app() {
-    let (app, driver) = App::<Tokio>::new("Hello Tokio!");
-    let driver_handle = tokio::spawn(async move { driver.run().await });
+    #[tokio::test]
+    async fn tokio_app() {
+        let (app, driver) = App::<Tokio>::new("Hello Tokio!");
+        let driver_handle = tokio::spawn(async move { driver.run().await });
 
-    let init_state = app.get_state().await.unwrap();
-    assert_eq!(init_state, "Hello Tokio!");
+        let init_state = app.get_state().await.unwrap();
+        assert_eq!(init_state, "Hello Tokio!");
 
-    let new_state = app.update_state("Hello again!").await.unwrap();
-    assert_eq!(new_state, "Hello again!");
+        let new_state = app.update_state("Hello again!").await.unwrap();
+        assert_eq!(new_state, "Hello again!");
 
-    app.terminate().await.unwrap();
-    let _ = driver_handle.await.unwrap();
+        app.terminate().await.unwrap();
+        let _ = driver_handle.await.unwrap();
+    }
 }
